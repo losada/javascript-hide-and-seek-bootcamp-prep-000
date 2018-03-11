530bcde5031ce5314b6f3626f5d7fd587a4c98b4
Michael Losada <michael.losada@gmail.com>
2018-03-11 19:51:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n){
+  var list = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0; i < list.length; i++){
+    list[i].innerHTML=parseInt(list[i].innerHTML)+n;
+  }
+}
+function deepestChild(){
+  var list = document.getElementById("grand-node").querySelectorAll("div");
+}
