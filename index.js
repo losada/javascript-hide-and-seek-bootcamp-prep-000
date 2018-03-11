318bd6839d647c7ee0db29f9d324137a1edf3850
Michael Losada <michael.losada@gmail.com>
2018-03-11 19:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ function increaseRankBy(n){
 }
 function deepestChild(){
   var list = document.getElementById("grand-node").querySelectorAll("div");
-  
+  var child = []
+  for(let i=0; i < child.length; i++){
+    child=
+  }
 }
